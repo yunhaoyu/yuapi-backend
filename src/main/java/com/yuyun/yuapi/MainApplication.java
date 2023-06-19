b5892c09bb3a04d672eb86ee698ab480daea4660
yunhaoyu <2018440280@qq.com>
2023-06-19 12:18:00 +0800

    nacos(注册中心) yuapi-backend(提供者) gateway(调用者) 调用调试完成

--- src/main/java/com/yuyun/yuapi/MainApplication.java
+++ src/main/java/com/yuyun/yuapi/MainApplication.java
@@ -1,5 +1,6 @@
 package com.yuyun.yuapi;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +18,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(exclude = {RedisAutoConfiguration.class})
 @MapperScan("com.yuyun.yuapi.mapper")
 @EnableScheduling
+@EnableDubbo
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class MainApplication {
 
--- yuapi-gateway/src/main/java/com/yuyun/yuapigateway/YuapiGatewayApplication.java
+++ yuapi-gateway/src/main/java/com/yuyun/yuapigateway/YuapiGatewayApplication.java
@@ -1,13 +1,36 @@
 package com.yuyun.yuapigateway;
 
+import com.yuyun.yuapi.provider.DemoService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
+@EnableDubbo
+@Service
 public class YuapiGatewayApplication {
 
+    @DubboReference
+    private DemoService demoService;
     public static void main(String[] args) {
-        SpringApplication.run(YuapiGatewayApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(YuapiGatewayApplication.class, args);
+        YuapiGatewayApplication application = context.getBean(YuapiGatewayApplication.class);
+        String result = application.doSayHello("world");
+        String result2 = application.doSayHello2("world");
+        System.out.println("result: " + result);
+        System.out.println("result: " + result2);
+    }
+
+    public String doSayHello(String name) {
+        return demoService.sayHello(name);
+    }
+
+    public String doSayHello2(String name) {
+        return demoService.sayHello2(name);
     }
 
 }
